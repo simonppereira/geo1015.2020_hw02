aea4daf068e27b2f9103a1a8c387cade5d1520e6
Daniel <danieldobsonmail@gmail.com>
2020-12-17 11:43:03 +0100

    testing a lot

--- python/my_code_hw02.py
+++ python/my_code_hw02.py
@@ -21,8 +21,9 @@ def Bresenham_with_rasterio(d, viewpoint,horizon_point):
      # d = rasterio dataset as above
      a = viewpoint
      b = horizon_point
-     ax, ay = a[0], a[1]
-     bx, by = b[0], b[1]
+     ax, ay = d.xy(a[0], a[1])
+     bx, by = d.xy(b[0], b[1])
+
      #-- create in-memory a simple GeoJSON LineString
      v = {}
      v["type"] = "LineString"
@@ -37,26 +38,37 @@ def Bresenham_with_rasterio(d, viewpoint,horizon_point):
      # re is a numpy with d.shape where the line is rasterised (values != 0)
      #line = numpy.nonzero(re == 1)
      #line = numpy.flip(line)
-     output = numpy.argwhere(re == 1)
-     output = output[output[:,0].argsort()[::-1]]
+     #output = numpy.argwhere(re == 1)
+     #output = output[output[:,0].argsort()[::-1]]
      #output = output[output[:,1].argsort()]
      #output = output[output[:,0].argsort()]
      #output = output.sort()
-     
-     if bx > ax:
+     output = numpy.argwhere(re == 1)
+
+     if bx >= ax and by >= ay:
         output = numpy.argwhere(re == 1)
-     elif bx < ax:
+        #print(ax, bx)
         output = output[output[:,0].argsort()[::-1]]
+        #output = numpy.fliplr(output)
+     elif bx < ax and by > ay:
+        output = numpy.argwhere(re == 1)
+        #output = numpy.fliplr(output)
+        output = output[output[:,0].argsort()[::-1]]
+     elif bx < ax and by < ay:
+        output = numpy.argwhere(re == 1)
+        output = output[output[:,1].argsort()[::-1]]
+     else:
+        output = numpy.argwhere(re == 1)
     
      # Sort on x high to low
      #output = output[output[:,0].argsort()[::-1]]
-     # Sort on x hlow to high
+     # Sort on x low to high
      #output = output[output[:,0].argsort()]
      # Sort on y low to high
      #output = output[output[:,1].argsort()]
      # Sort on y high to low
      #output = output[output[:,1].argsort()[::-1]]
-     return output
+     return output, re
 
 
 def slope(d,v,h,q,z):
@@ -71,7 +83,8 @@ def slope(d,v,h,q,z):
     z:              z value point on line        
 
     Output:
-    a:              slope   
+    a:              slope 
+    x:              distance  
     """
     y = z
     b = h
@@ -87,13 +100,13 @@ def tangent_curr(a,x,b):
 
     Inputs:
     a:              slope
-    x:              distance (delta x)
+    x:              x coordinate point
     b:              viewpoint height 
 
     Outputs:
     y:              tangent height
     """
-    y = a*x + b
+    y = (a*x) + b
     return y
 
 def output_viewshed(d, viewpoints, maxdistance, output_file):
@@ -133,16 +146,16 @@ def output_viewshed(d, viewpoints, maxdistance, output_file):
     #v2 = viewpoints[1]
     print('viewpoint', v)
     #print('viewpoint', v2)
-    #print('viewpoint x', v[0])
-    #print('viewpoint y', v[1])
-    h = viewpoints[0][2]
-    print('height', h)
+    print('viewpoint x', v[0])
+    print('viewpoint y', v[1])
+    #h = viewpoints[0][2]
+    #print('height', h)
 
     #cellsize 
     cell_ul,_ = d.xy(v[0],v[1],offset='ul')
     cell_ur,_ = d.xy(v[0],v[1],offset='ur')
     cellsize = cell_ur-cell_ul
-    #print('cellsize', cellsize)
+    print('cellsize', cellsize)
 
     #-- Radius of viewpoint
     radius = maxdistance
@@ -160,38 +173,110 @@ def output_viewshed(d, viewpoints, maxdistance, output_file):
     # 1: visible from the viewpoint(s)
     # 2: the pixel contains a viewpoint
     # 3: the pixel is outside the max-distance/horizon zone(s)
-       
-    
+
+    #v = i
+    # index of this point in the numpy raster
+    vi = vrow, vcol = d.index(v[0], v[1])
+    print('vi in beginning loop',vi)
+    #vi = vrow, vcol
+    # This is the value of the centerpoint of the viewshed
+    h = npi[vrow][vcol] + v[2]
+    print('height',h)
+    npvs[vrow,vcol] = 2 
+    horizon_points = []
+    output_list= []
+    vi_list = []
+    xy_lists = []
+    for row in enumerate(npvs):
+        row_i = row[0]
+        for col in enumerate(row[1]):
+            col_i = col[0]
+            pt = x,y = d.xy(row_i,col_i)
+            dist = distance(v,pt)
+            if dist > (radius - cellsize) and dist < radius:
+                #npvs[row_i,col_i] = 1
+                horizon_point = (row_i, col_i)
+                horizon_points.append(horizon_point)
+                vi_list.append(vi)
+                output,_ = Bresenham_with_rasterio(d,vi,horizon_point)
+                #print(output)
+                output_list.append(output)
+                    
+                slope_last = [-100]
+                xy_list = []
+                count = 0
+                for point in output:
+                    #print(output)
+                    count += 1
+                    cell_pt = cx, cy = point[0], point[1]
+                    #print(cx,cy)
+                    npvs[cx][cy] = count
+                    #xy_list.append(cell_pt)
+                    #print(slope_last[-1])
+                    #print(x, y)
+                    cell_cx, cell_cy = d.xy(cx, cy)
+                    #npvs[cx,cy] = 1
+                    slope_curr, dist_x = slope(d,v,h,cell_pt,npi[cx][cy])
+                    y_curr = tangent_curr(slope_last[-1],dist_x,h)
+                    #print(y_curr)
+                    #dont use now t_curr = slope(d,v,h,cell_pt,npi[x][y])
+                    '''
+                    if npi[cx][cy] < y_curr:                      
+                        npvs[cx,cy] = 0
+                    elif npi[cx][cy] >= y_curr:
+                        npvs[cx,cy] = 1
+                        #a_curr,_ = slope(d,v,h,cell_pt,npi[cx][cy])
+                        slope_last.append(slope_curr)
+                    '''
+                #xy_lists.append(xy_list)
+
+    '''            
+    for i in range(0,2):
+        print('horizon points', horizon_points[i])
+        print('output list', output_list[i])
+        print('vi',vi_list[i])
+        print('xy in output', xy_lists[i])
+    '''
+
+    '''
     for i in viewpoints:
         v = i
         # index of this point in the numpy raster
         vi = vrow, vcol = d.index(v[0], v[1])
+        print('vi in beginning loop',vi)
         #vi = vrow, vcol
         # This is the value of the centerpoint of the viewshed
-        h = v[2]
-        #print('height',h)
-        npvs[vrow , vcol] = h 
-        npvs
-
+        h = npi[vrow][vcol] + v[2]
+        print('height',h)
+        npvs[vrow,vcol] = 2 
+        horizon_points = []
+        output_list= []
+        vi_list = []
         for row in enumerate(npvs):
             row_i = row[0]
             for col in enumerate(row[1]):
                 col_i = col[0]
                 pt = d.xy(row_i,col_i)
                 dist = distance(v,pt)
-                if dist > (radius - cellsize) and dist < radius and npvs[row_i,col_i] != 2:
+                if dist > (radius - cellsize) and dist < radius:
                     #npvs[row_i,col_i] = 1
                     horizon_point = (row_i, col_i)
-                    output = Bresenham_with_rasterio(d,vi,horizon_point)
-                    #print(output) 
+                    horizon_points.append(horizon_point)
+                    vi_list.append(vi)
+                    output,_ = Bresenham_with_rasterio(d,vi,horizon_point)
+                    #print(output)
+                    output_list.append(output)
+                     
                     slope_last = [-100]
+                    
                     for point in output:
+                        #print(output)
                         cell_pt = x, y = point[0], point[1]
                         #print(x, y)
+                        dx,_ = d.xy(x, y)
                         _, dist_x = slope(d,v,h,cell_pt,npi[x][y])
                         y_curr = tangent_curr(slope_last[-1],dist_x,h)
                         #print(y_curr)
-                        #print(npi[x][y])
                         #dont use now t_curr = slope(d,v,h,cell_pt,npi[x][y])
                         if npi[x][y] < y_curr:                      
                             npvs[x,y] = 0
@@ -201,7 +286,45 @@ def output_viewshed(d, viewpoints, maxdistance, output_file):
                             slope_last.append(a_curr)
                     #print(slope_last)
     
+        npvs[vrow,vcol] = 2
+                
+    print('length horizon points',len(horizon_points))
 
+    
+    vt = viewpoints[0]
+    vti = vtrow, vtcol = d.index(vt[0], vt[1])
+    print('vti', vti)
+    test_line1,test_re1 = Bresenham_with_rasterio(d,vti,horizon_points[0])
+    test_line2,test_re2 = Bresenham_with_rasterio(d,vti,horizon_points[13])
+    test_line3,test_re3 = Bresenham_with_rasterio(d,vti,horizon_points[177])
+    test_line4,test_re4 = Bresenham_with_rasterio(d,vti,horizon_points[180])
+    print(test_line1)
+    print(test_line2)
+    print(test_line3)
+    print(test_line4)
+    print('horizon point 0',horizon_points[0])
+    print('horizon point 13',horizon_points[13])
+    print('horizon point 177',horizon_points[177])
+    print('horizon point 180',horizon_points[180])
+    print('vti', vti)
+    cell_walk = v[0], v[1]+cellsize
+    print('cell_walk coordinates',cell_walk)
+    cell_walk_i = d.index(cell_walk[0], cell_walk[1])
+    print('cell walk indices', cell_walk_i)
+    print('dataset corresponding indices for cell walk coordinates: ', d.index(cell_walk[0],cell_walk[1]))
+    #print(cell_walk in test_line)
+    #npvs[341,320] = 2
+    print('output list', output_list[0])
+    print('output list', output_list[1])
+    #print([340, 320] in output_list[0])
+    
+    for i in range(0,2):
+        print('horizon points', horizon_points[i])
+        print('output list', output_list[i])
+        print('vi',vi_list[i])
+    '''
+    print('first vi',vi_list[0])
+    print('flast vi',vi_list[-1])
     #-- write this to disk
     with rasterio.open(output_file, 'w', 
                        driver='GTiff', 
@@ -212,7 +335,48 @@ def output_viewshed(d, viewpoints, maxdistance, output_file):
                        crs=d.crs, 
                        transform=d.transform) as dst:
         dst.write(npvs.astype(rasterio.uint8), 1)
+    '''
+    with rasterio.open('test_re1.tif', 'w', 
+                    driver='GTiff', 
+                    height=npi.shape[0],
+                    width=npi.shape[1], 
+                    count=1, 
+                    dtype=rasterio.uint8,
+                    crs=d.crs, 
+                    transform=d.transform) as dst:
+        dst.write(test_re1.astype(rasterio.uint8), 1)
     
+    with rasterio.open('test_re2.tif', 'w', 
+                    driver='GTiff', 
+                    height=npi.shape[0],
+                    width=npi.shape[1], 
+                    count=1, 
+                    dtype=rasterio.uint8,
+                    crs=d.crs, 
+                    transform=d.transform) as dst:
+        dst.write(test_re2.astype(rasterio.uint8), 1)
+
+    with rasterio.open('test_re3.tif', 'w', 
+                    driver='GTiff', 
+                    height=npi.shape[0],
+                    width=npi.shape[1], 
+                    count=1, 
+                    dtype=rasterio.uint8,
+                    crs=d.crs, 
+                    transform=d.transform) as dst:
+        dst.write(test_re3.astype(rasterio.uint8), 1)
+    
+    with rasterio.open('test_re4.tif', 'w', 
+                    driver='GTiff', 
+                    height=npi.shape[0],
+                    width=npi.shape[1], 
+                    count=1, 
+                    dtype=rasterio.uint8,
+                    crs=d.crs, 
+                    transform=d.transform) as dst:
+        dst.write(test_re4.astype(rasterio.uint8), 1)
+    '''
+
     print("Viewshed file written to '%s'" % output_file)
 
 
